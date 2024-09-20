6f5bb84b410d9899126362b199d8ab6af45ba601
Hemalatha <hemalatha292004@gmail.com>
2024-09-20 13:45:06 +0530

    update

--- src/Content.js
+++ src/Content.js
@@ -1,20 +1,21 @@
 import React from "react";
+import { useState } from "react";
 const Content = () => {
     function handleNameChange(){
         const names=["earn","grow","give"]
         const int=Math.floor(Math.random()*3);
         return names[int];
     }
-    const handleClick =() =>{
-        console.log('thanks for the support')
+    const handleClick =(e) =>{
+        console.log(e.target.innerText)
     }
     const handleClick2 =(name) =>{
         console.log(`thanks for the support ${name}`)
     }
   return (
     <main>
-    <p>let {handleNameChange()} Money</p>
-    <button onClick={()=>handleClick2('hema')}>Subscribe</button>
+    <p onDoubleClick={()=>handleClick2('sri')}>let {handleNameChange()} Money</p>
+    <button onClick={(e)=>handleClick(e)}>Subscribe</button>
     </main>
   )
 }
